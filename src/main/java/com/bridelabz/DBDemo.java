f704fc71ec45c11cfaf7e010d0df5787a8d5f3c8
bhavani125 <girinenibhavani12@gmil.com>
2021-08-16 17:48:41 +0530

    Added:Welcome message

---
+++ src/main/java/com/bridelabz/DBDemo.java
@@ -0,0 +1,8 @@
+package com.bridelabz;
+
+
+public class DBDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee payroll service using jdbc");
+    }
+}
